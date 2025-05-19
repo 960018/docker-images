c2632ebe750b8eebbf38f12873a82a2a1e4516ed
k0d3r1s <k0d3r1s@gmail.com>
2025-05-19 15:22:34 +0300

    fix: fix entrypoints [skip ci]

--- debian/env_entrypoint.sh
+++ debian/env_entrypoint.sh
@@ -6,9 +6,7 @@ if [ "$(id -u)" = "0" ]; then
     if [ -S /var/run/docker.sock ]; then
         SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
 
-        if ! getent group docker >/dev/null; then
-            groupadd -g "$SOCK_GID" docker || true
-        fi
+        groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
 
         usermod -aG "$SOCK_GID" vairogs || true
     fi
