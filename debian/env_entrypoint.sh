970ff2b1b44f9a7857201232ebd2f5e58834339c
k0d3r1s <k0d3r1s@gmail.com>
2025-05-19 18:41:10 +0300

    fix: loop [skip ci]

--- debian/env_entrypoint.sh
+++ debian/env_entrypoint.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
+if [ -z "$REEXEC_DONE" ]; then
+    SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
 
-if [ "$(id -u)" = "0" ]; then
-    groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
-    usermod -aG "$SOCK_GID" vairogs || true
-    exec "$@"
-else
-    sudo groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
-    sudo usermod -aG "$SOCK_GID" vairogs || true
-    exec su - vairogs -c "exec \"$0\" \"$@\""
+    if [ "$(id -u)" = "0" ]; then
+        groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
+        usermod -aG "$SOCK_GID" vairogs || true
+    else
+        sudo groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
+        sudo usermod -aG "$SOCK_GID" "$(whoami)" || true
+
+        export REEXEC_DONE=1
+        exec su - "$(whoami)" -c "exec \"$0\" \"$@\""
+    fi
 fi
+
+exec "$@"
