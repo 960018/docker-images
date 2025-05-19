92d53fe2acc610d21077db4be179dae1fd90f1f8
k0d3r1s <k0d3r1s@gmail.com>
2025-05-19 20:46:16 +0300

    fix: final entrypoint

--- go/env_entrypoint.sh
+++ go/env_entrypoint.sh
@@ -2,18 +2,17 @@
 
 set -e
 
-if [ "$(id -u)" = "0" ]; then
-    if [ -t 0 ] && [ -t 1 ]; then
-        exec "$@"
-    fi
+if [ -z "$REEXEC_DONE" ]; then
+    SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
 
-    if [ -S /var/run/docker.sock ]; then
-        SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
+    if [ "$(id -u)" = "0" ]; then
         groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
         usermod -aG "$SOCK_GID" vairogs || true
+    else
+        sudo groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
+        sudo usermod -aG "$SOCK_GID" "$(whoami)" || true
+        exec sudo -u "$(whoami)" env REEXEC_DONE=1 "$0" "$@"
     fi
-
-    exec su - vairogs -c "exec script -q -c '$0 $*' /dev/null"
 fi
 
 if [ -f /home/vairogs/environment/environment.txt ]; then \
