d65c747c85cb0eb250283212f6f5e8abcdaab782
k0d3r1s <k0d3r1s@gmail.com>
2025-09-15 12:46:14 +0300

    Simplifies entrypoint scripts
    Removes unnecessary user and group modification logic from the entrypoint scripts.
    
    The debian entrypoint script is simplified to directly execute the given command.

--- debian/env_entrypoint.sh
+++ debian/env_entrypoint.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$REEXEC_DONE" ]; then
-    SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
-
-    if [ "$(id -u)" = "0" ]; then
-        groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
-        usermod -aG "$SOCK_GID" vairogs || true
-    else
-        sudo groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
-        sudo usermod -aG "$SOCK_GID" "$(whoami)" || true
-        exec sudo -E -u "$(whoami)" env REEXEC_DONE=1 "$0" "$@"
-    fi
-fi
-
 exec "$@"
--- php/env_entrypoint.sh
+++ php/env_entrypoint.sh
@@ -2,19 +2,6 @@
 
 set -e
 
-if [ -z "$REEXEC_DONE" ]; then
-    SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
-
-    if [ "$(id -u)" = "0" ]; then
-        groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
-        usermod -aG "$SOCK_GID" vairogs || true
-    else
-        sudo groupadd -o -g "$SOCK_GID" tempdocker 2>/dev/null || true
-        sudo usermod -aG "$SOCK_GID" "$(whoami)" || true
-        exec sudo -E -u "$(whoami)" env REEXEC_DONE=1 "$0" "$@"
-    fi
-fi
-
 if [ -f /home/vairogs/environment/environment.txt ]; then \
 	while IFS= read -r line; do \
 		export "$line"; \
