0b0da87841c075eaa3a57e1a2a5eacc036c18e43
k0d3r1s <k0d3r1s@gmail.com>
2025-10-18 05:18:30 +0300

    simplify preload

--- php/preload.php
+++ php/preload.php
@@ -1,39 +1,9 @@
 <?php declare(strict_types = 1);
 
-function bool(
-    mixed $value,
-): bool {
-    if (is_bool($value)) {
-        return $value;
-    }
+if (false === ($_ENV['DISABLE_PRELOAD'] ?? false)) {
+    $preloadFile = $_ENV['PRELOAD_FILE'] ?? '/var/www/html/config/preload.php';
 
-    $value = strtolower((string) $value);
-
-    return match ($value) {
-        'y', '1', 'true' => true,
-        'n', '0', 'false' => false,
-        default => filter_var($value, FILTER_VALIDATE_BOOL),
-    };
-}
-
-if ('cli' !== PHP_SAPI && bool($_ENV['ENABLE_PRELOAD'] ?? true)) {
-    if (null === ($_ENV['PRELOAD_FILE'] ?? null)) {
-        if ('prod' === ($_ENV['APP_ENV'] ?? null)) {
-            $prefix = '/var/www/html';
-            $path = '/var/cache/prod/';
-            $filename = 'App_KernelProdContainer.preload.php';
-
-            if (!is_dir($long = $prefix . ($_ENV['CUSTOM_PRELOAD_PREFIX'] ?? '/custom') . $path)) {
-                $long = null;
-            }
-
-            $dir = $_ENV['PRELOAD_DIR'] ?? (is_dir($short = $prefix . $path) ? $short : $long);
-
-            if (null !== $dir && file_exists($dir . $path . $filename)) {
-                opcache_compile_file($dir . $path . $filename);
-            }
-        }
-    } else {
-        opcache_compile_file($_ENV['PRELOAD_FILE']);
+    if ('cli' !== PHP_SAPI && is_file($preloadFile)) {
+        require $preloadFile;
     }
 }
